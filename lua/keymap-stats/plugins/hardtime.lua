93d27b95afd6138524c7f61a77ebe65592704b67
Gonzalo Matheu (framework) <gonzalommj@gmail.com>
2024-12-30 13:12:54 -0300

    Explicitly loading hardtime before instrumentation

--- lua/keymap-stats/plugins/hardtime.lua
+++ lua/keymap-stats/plugins/hardtime.lua
@@ -1,11 +1,19 @@
 local M = {}
+local state = {
+  instrumented = false,
+}
+local stats = {}
 
 --luacheck: ignore 212 unused argument
-local function instrument_hardtime(count, count_keymap, notify)
-  -- local hardtime = require("hardtime")
+local function instrument_hardtime(_count, _count_keymap, notify, opts)
+  local hardtime = require("hardtime")
+  hardtime.disable()
+  hardtime.enable()
+  state.instrumented = true
 end
 
 M.setup = instrument_hardtime
-M.stats = {}
+M.state = state
+M.stats = stats
 
 return M
